520bb7a2224840dd5a6ed395e88e9093777a03fc
可以癫 <rikmorrrr@gmail.com>
2023-08-17 18:34:20 +0800

    fix macro error

--- Source/ARPGDemo/DebugMacro.h
+++ Source/ARPGDemo/DebugMacro.h
@@ -1,10 +1,7 @@
 #include "CoreMinimal.h"
 
-#define DRAW_SPHERE                                                                      \
-    (Location)                                                                           \
-    {                                                                                    \
-        if (GetWorld())                                                                  \
-        {                                                                                \
-            DrawDebugSphere(GetWorld(), Location, 30, 10, FColor::Emerald, false, 5, 2); \
-        }                                                                                \
+#define DRAW_SPHERE(Location)                                                        \
+    if (GetWorld())                                                                  \
+    {                                                                                \
+        DrawDebugSphere(GetWorld(), Location, 30, 10, FColor::Emerald, false, 5, 2); \
     }
