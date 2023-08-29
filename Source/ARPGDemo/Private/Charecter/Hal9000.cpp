15948dd7d4411d82c2009c70b8cc0fba4140ff67
可以癫 <rikmorrrr@gmail.com>
2023-08-29 18:15:55 +0800

    hal9000 input

---
+++ Source/ARPGDemo/Private/Charecter/Hal9000.cpp
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Charecter/Hal9000.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AHal9000::AHal9000()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationPitch =false;
+	bUseControllerRotationRoll=false;
+	bUseControllerRotationYaw=false;
+
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+	
+	SpringArm=CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength=300.f;
+	SpringArm->bUsePawnControlRotation=true;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Main Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void AHal9000::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AHal9000::MoveForward(float Value) 
+{
+	if(!Controller || Value==0) return;;
+	
+	//youtube video version.
+	const FRotator ControlRotation (0.f,GetControlRotation().Yaw,0.f);
+	const FVector Direction = FRotationMatrix(ControlRotation).GetUnitAxis(EAxis::X);
+	
+	// my direction version.
+	// const FVector Forward =SpringArm->GetForwardVector();
+	AddMovementInput(Direction,Value);
+}
+
+void AHal9000::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AHal9000::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void AHal9000::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHal9000::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent ->
+		BindAxis(FName("MoveForward"),this,&AHal9000::MoveForward);
+
+	PlayerInputComponent->
+	BindAxis(FName("Turn"),this,&AHal9000::Turn);
+
+	PlayerInputComponent->
+	BindAxis(FName("LookUp"),this,&AHal9000::LookUp);
+}
+
+
+
---
+++ Source/ARPGDemo/Public/Charecter/Hal9000.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Hal9000.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class ARPGDEMO_API AHal9000 : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AHal9000();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* Camera;
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArm;
+	
+	void MoveForward(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+};
