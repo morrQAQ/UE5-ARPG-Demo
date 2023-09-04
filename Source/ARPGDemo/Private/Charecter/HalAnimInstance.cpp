7e7a8d31b983e554cc855ed1b708cd206746365c
可以癫 <rikmorrrr@gmail.com>
2023-09-04 11:41:34 +0800

    //TODO ik-foot-i-hate-blueprint

--- Source/ARPGDemo/Private/Charecter/HalAnimInstance.cpp
+++ Source/ARPGDemo/Private/Charecter/HalAnimInstance.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Charecter/HalAnimInstance.h"
 #include "Charecter/Hal9000.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -12,10 +9,7 @@ void UHalAnimInstance::NativeInitializeAnimation()
 
 	hal9000 = Cast<AHal9000>(TryGetPawnOwner());
 
-	if (!hal9000)
-	{
-		return;
-	}
+	if (!hal9000) { return; }
 
 	Movement = hal9000->GetCharacterMovement();
 }
@@ -24,10 +18,7 @@ void UHalAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
 
-	if (!Movement)
-	{
-		return;
-	}
+	if (!Movement) { return; }
 
 	GroundSpeed = UKismetMathLibrary::VSizeXY(Movement->Velocity);
 	IsFalling = Movement->IsFalling();
